ed4707ee5bd96020c4ee0a01f9dbcd137817902b
Sdywolf <chnfy@stu.pku.edu.cn>
2024-05-20 02:03:26 +0800

    Re-organize module hierarchy

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,12 @@
-use lalrpop_util::lalrpop_mod;
 use std::io;
-mod ast;
-use crate::ast::{Sentence, Var, Ident};
-
-lalrpop_mod!(parser);
+mod utils;
+use utils::parser::get_token;
+use utils::ast::{Sentence, Var, Ident};
 
 static mut VAR_TABLE : Vec<Var> = Vec::new();
 
 fn main() {
-    let mut input = String::new();
     loop{
-        input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        let res = parser::StmtParser::new().parse(&input).unwrap();
-
-        match &res {
-            Sentence::Assignment(id, expr) => {
-                match id {
-                    Ident::Null => { expr.calculate().print(); },
-                    Ident::Name(name) => {
-                        let mut cnt = 0;
-                        unsafe {
-                            while cnt < VAR_TABLE.len() {
-                                if VAR_TABLE[cnt].name == *name {
-                                    break;
-                                }
-                                cnt = cnt + 1;
-                            }
-                            
-                            if cnt < VAR_TABLE.len() {
-                                VAR_TABLE[cnt] = Var{
-                                    name : name.to_string(), 
-                                    value : expr.calculate(), 
-                                };
-                                VAR_TABLE[cnt].print();
-                            }
-                            else {
-                                VAR_TABLE.push(Var{
-                                    name : name.to_string(), 
-                                    value : expr.calculate(), 
-                                });
-                                VAR_TABLE[VAR_TABLE.len() - 1].print();
-                            }
-                        }
-                    },
-                }
-            }
-        }
+        get_token();
     }
 }
